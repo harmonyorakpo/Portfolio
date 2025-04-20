fd73e6204e750f0518a296b24549496a516ba0fd
HARMONY <harmony.etas@maxfameltd.com>
2025-04-21 00:52:16 +0100

    implemented the download cv feature

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,13 @@
 // import React from 'react'
 
 const Hero = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = `public/cv/Harmony_Orakpoyovwuru_cv.pdf`;
+    link.download = "Harmony Orakpoyovwuru CV.pdf";
+    link.click();
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-[calc(100vh-60px)] text-center">
@@ -16,8 +23,7 @@ const Hero = () => {
 
         <div className="mt-8 flex gap-4">
           <button
-            // href="/assets/cv/Harmony_Orakpoyovwuru_cv.pdf.pdf"
-            // download
+            onClick={handleDownload}
             className="bg-primary text-white px-6 py-2 rounded-sm font-medium"
           >
             My Resume
@@ -30,6 +36,5 @@ const Hero = () => {
     </>
   );
 };
-
 
 export default Hero;
